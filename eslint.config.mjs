e61e0bbed93e636e638a59f6f17cc58978c16f68
jdhopia <jhopia19@outlook.com>
2025-02-13 16:16:14 -0600

    Fixed ESLint config for Vercel deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+/** @type {import('eslint').Linter.Config} */
+const config = {
+  extends: ["next/core-web-vitals"],
+};
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+export default config;
